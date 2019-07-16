d4cfff16b61d2d01b8706c8b30c7b69ee0970162
Mahdi_Moradi19 <Mahdi_Moradi@yahoo.com>
2019-07-16 14:29:26 +0430

    Make a feature that can auto crate Admin table just by name of database :)

--- AdminPanel/control/db.php
+++ AdminPanel/control/db.php
@@ -4,6 +4,14 @@
 	$db = @mysqli_connect("$host", "$user", "$pass" , "$dbname");
 	if(!mysqli_connect_error())
 	{
+		if(!file_exists('OK.f'))
+		{
+			mysqli_query($db, "CREATE TABLE `$dbname`.`Admin` ( `firstname` TEXT NOT NULL , `lastname` TEXT NOT NULL , `username` TEXT NOT NULL , `password` TEXT NOT NULL , `description` TEXT NOT NULL , `picture` TEXT NOT NULL , `email` TEXT NOT NULL )");
+			mysqli_query($db, "INSERT INTO `admin` (`firstname`, `lastname`, `username`, `password`, `description`, `picture`, `email`) VALUES ('Mahdi', 'Moradi', 'Mahdi_Moradi', '1234', 'i am mahdi', 'mahdi.jpg', 'mahdi@mahdi.com') ");
+			mysqli_query($db, "INSERT INTO `admin` (`firstname`, `lastname`, `username`, `password`, `description`, `picture`, `email`) VALUES ('Ahmad', 'Ahmadi', 'Ahmad_Ahmadi', '5678', 'i am ahmad', 'ahmad.jpg', 'ahmad@ahmad.com') ");
+			$f = fopen('OK.f', 'w');
+			fwrite($f,'DONT DELETE THIS FILE AT ALL!');
+		}
 		$status = true;
 	}
 ?>
