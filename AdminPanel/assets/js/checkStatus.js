09b83b2c2a07f2056442d61512d80205a6ea0788
Mahdi_Moradi19 <Mahdi_Moradi@yahoo.com>
2019-07-16 12:40:36 +0430

    Slove Problem of intract with user in login page(affect on checkStatus.js and login.html)

---
+++ AdminPanel/assets/js/checkStatus.js
@@ -0,0 +1,10 @@
+st = window.location.toString();
+st = st.charAt(st.length - 1);
+if(st == 'i')
+{
+	alert("Sorry, your information is incorrect!\nPlease try again!");
+}
+else if(st == 'e')
+{
+	alert("You must Enter someword in both Boxes!");
+}
